232035ab3a5ca14246efc01a15e4883dbdae9f8b
Onam Kwon <kon6443@gmail.com>
2022-08-19 17:20:41 +0900

    online user list available

--- controllers/chat/connectSocket.js
+++ controllers/chat/connectSocket.js
@@ -10,16 +10,19 @@ require('dotenv').config();
 
 module.exports = (server) => {
     const io = SocketIO(server, {path: '/socket.io'});
+    let userDict = {};
     io
     .use((socket, next) => {
         cookieParser()(socket.request, socket.request.res || {}, next);
     })
     .on('connection', function (socket) {
         const req = socket.request;
-        const decoded = jwt.verify(req.cookies.user, process.env.SECRET_KEY);
-        socket.name = decoded.id;
+        const user = jwt.verify(req.cookies.user, process.env.SECRET_KEY);
+        socket.name = user.id;
+        userDict[socket.id] = user.id;
         console.log(socket.id, ' connected: ', socket.name);
     
+        io.emit('online', userDict);
         // broadcasting a entering message to everyone who is in the chatroom
         io.emit('msg', `${socket.name} has entered the chatroom.`);
 
@@ -32,6 +35,8 @@ module.exports = (server) => {
 
         // user connection lost
         socket.on('disconnect', function (data) {
+            delete userDict[socket.id];
+            io.emit('online', userDict);
             io.emit('msg', `${socket.name} has left the chatroom.`);
         }); 
     });

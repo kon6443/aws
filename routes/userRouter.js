656f372131793737296db21d31936fa64bc997e4
Onam Kwon <kon6443@gmail.com>
2022-08-29 16:31:26 +0900

    organizing

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,14 +4,14 @@ const express = require("express");
 const router = express.Router();
 
 // Importing controller
-const loginMiddleWare = require('../controllers/user/user.controller');
+const userMiddleWare = require('../controllers/user/user.controller');
 
 const auth = require("../controllers/authMiddleware");
 router.use('/', auth);
 
-router.get('/', loginMiddleWare.showMain);
-router.post('/:id/:address/:pw/:pwc', loginMiddleWare.signUp);
-router.post('/:id/:pw', loginMiddleWare.signIn);
-router.delete('/logout', loginMiddleWare.signOut);
+router.get('/', userMiddleWare.showMain);
+router.post('/:id/:address/:pw/:pwc', userMiddleWare.signUp);
+router.post('/:id/:pw', userMiddleWare.signIn);
+router.delete('/logout', userMiddleWare.signOut);
 
 module.exports = router;

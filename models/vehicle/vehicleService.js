daf1be0875ef1c2dc1e88b6420a8e2430cd14aea
Onam Kwon <kon6443@gmail.com>
2023-02-09 20:36:26 +0900

    Adding vechile information service.

--- models/vehicle/vehicleService.js
+++ models/vehicle/vehicleService.js
@@ -5,11 +5,11 @@ const rp = require('request-promise');
 
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') }); 
 
-exports.showMain = (req, res, next) => {
+exports.showMain = async (req, res, next) => {
     return res.sendFile(path.join(__dirname, '../../views/vehicle/vehicleMain.html'));
 }
 
-exports.getVehicleInfo = (req, res, next) => {
+exports.getVehicleInfo = async (req, res, next) => {
     const {
         registYy, 
         registMt, 
@@ -18,6 +18,23 @@ exports.getVehicleInfo = (req, res, next) => {
         prye
     } = req.query;
     console.log(req.query);
+
+    var url = 'http://apis.data.go.kr/B553881/prfomncChckInfoService/prfomncChckInfoService';
+    var queryParams = '?' + encodeURIComponent('serviceKey') + '='+process.env.PI_SERVICE_ENCODE; /* Service Key*/
+    queryParams += '&' + encodeURIComponent('registYy') + '=' + encodeURIComponent(registYy);
+    queryParams += '&' + encodeURIComponent('registMt') + '=' + encodeURIComponent(registMt);
+    queryParams += '&' + encodeURIComponent('vhctyAsortCode') + '=' + encodeURIComponent(vhctyAsortCode);
+    queryParams += '&' + encodeURIComponent('registGrcCode') + '=' + encodeURIComponent(registGrcCode);
+    queryParams += '&' + encodeURIComponent('prye') + '=' + encodeURIComponent(prye);
+    const options = {
+        url: url+queryParams,
+        method: 'GET'
+    }
+    const body = await rp(options);
+    console.log(body);
+    // vhctyAsortCode: 3
+    // registGrcCode: 9
+    // 
 }
 
 exports.errorHandler = (err, req, res, next) => {

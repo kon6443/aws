efdf8bfb70f7ec65cd8ef7a8cd565252cb24dedd
Onam Kwon <kon6443@gmail.com>
2023-03-06 22:10:27 +0900

    Code refactoring.

--- controllers/tetris/tetrisRouter.js
+++ controllers/tetris/tetrisRouter.js
@@ -1,11 +1,16 @@
+/**
+ * tetrisRouter.js
+ */
+
 const express = require("express");
 const app = express();
 const router = express.Router();
-
-const tetrisMiddleWare = require('../../models/tetris/tetrisService');
+const path = require('path');
 
 app.use(express.static(__dirname + '/public'));
 
-router.get('/', tetrisMiddleWare.startGame);
+router.get('/', () => {
+    return res.sendFile(path.join(__dirname, '../../views/Tetris/tetris.html'));
+});
 
 module.exports = router;

8d5b61aac9131f57b29c21285c01b202c69d79aa
Onam Kwon <kon6443@gmail.com>
2023-04-29 17:24:44 +0900

    Code refactoring - articles.

--- controllers/articles/articleRouter.js
+++ controllers/articles/articleRouter.js
@@ -11,13 +11,10 @@ const ArticleControllerInstance = container.get('ArticleController');
 
 // '/articles'
 router.get('/', ArticleControllerInstance.handleMainRequest);
-// router.get('/write', FilterInstance.authenticationMethodDistinguisher, ArticleControllerInstance.handleGetArticleForm);
 router.get('/auto-completion', ArticleControllerInstance.handleGetAutoComplete);
 router.get('/new', FilterInstance.authenticationMethodDistinguisher, ArticleControllerInstance.handleGetWritingArticleForm);
 router.get('/:id', FilterInstance.authenticationMethodDistinguisher, ArticleControllerInstance.handleGetArticle);
 router.get('/:id/edit', FilterInstance.authenticationMethodDistinguisher, ArticleControllerInstance.handleGetEditingArticleForm);
-
-// router.get('/:resourceType/:id', FilterInstance.authenticationMethodDistinguisher, ArticleControllerInstance.handleGetArticleForm2);
 
 // Here resourceType is a placeholder. Depends on its value, routers handle an article or comment.
 router.post('/:resourceType/:id', FilterInstance.authenticationMethodDistinguisher, ArticleControllerInstance.handlePostResource);

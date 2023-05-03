1dc9ddfccefd14a74b8eec3d5b168f1f953b90fa
Onam Kwon <kon6443@gmail.com>
2023-05-03 11:23:09 +0900

    Deleted junk code.

--- controllers/home/homeRouter.js
+++ controllers/home/homeRouter.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const path = require('path');
 
 const container = require('../../models/container/container');
-const homeServiceInstacnce = container.get('homeService');
 const HomeControllerInstance = container.get('HomeController');
-
-// router.use('/', auth);
 
 // Home page.
 router.get('/', HomeControllerInstance.handleGetMainPage);
